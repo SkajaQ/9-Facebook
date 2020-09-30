01cf3fb4f2e6cd4d82bbd91fffa95941ab073645
SkajaQ <70883314+SkajaQ@users.noreply.github.com>
2020-09-30 12:53:55 +0300

    export/import

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const posts = [];
+
+
+const post = [
+    {
+        author: 'vard pav',
+        authorImage: './img/users/....',
+        time: '2020/10/01 16:48:48',
+        text: 'rr',
+        images: [
+            './kkk'
+        ],
+        background: 'red'
+    }
+];
+
+
+// autorius 
+//     nuotrauka
+//     vardas
+//     pavarde
+//     link
+// postTimeStamp  //miliseconds nuo 1970-01-01 00:00:00
+// turinys
+//     tekstas
+//     nuotrauku sarasas
+//     fonas
+
+
+
+
+export default data.js; 
